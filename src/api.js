b6e7cde13362de62baada91aa0da5c4f2f74a980
Sebastian Rodriguez <red7806@gmail.com>
2023-02-03 18:49:40 -0400

    online server serves content in https now

--- src/api.js
+++ src/api.js
@@ -32,7 +32,7 @@ const dummySource = {
 export const getData = async (lng, lat, rad) => {
     const serverOn = true
     // these urls are for me; for when im testing between my laptop or phone since flask servers are problematic lol
-    const onlineServer = 'http://150.230.170.110:5000/'
+    const onlineServer = 'https://150.230.170.110:5000/'
     const localFLaskURL = 'http://127.0.0.1:5000/'
     try {
         const response = serverOn ? await axios.get(onlineServer, {
